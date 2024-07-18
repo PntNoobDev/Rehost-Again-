25008e07fb886c1cd962c6bb2a523c8b1ab2fdcf
PntNoobDev <phamngocthanh.d21ctc1@muce.edu.vn>
2024-07-18 16:44:36 +0700

    update

--- Rehost(Again)/MainWindow.xaml.cs
+++ Rehost(Again)/MainWindow.xaml.cs
@@ -1,55 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 using System.Activities;
-using System.Activities.Core.Presentation;
 using System.Activities.Presentation;
-using System.Activities.Presentation.Metadata;
 using System.Activities.Presentation.Toolbox;
 using System.Activities.Statements;
-using System.ComponentModel;
-using System.Data.SqlTypes;
-
+using System.Activities.Core.Presentation;
+using System.Activities.Hosting;
+using System.Activities.Presentation.Services;
 
 namespace Rehost_Again_
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private WorkflowDesigner wd;
-        private Sequence sequence;
+        private WorkflowApplication wfApp;
+        private bool isWorkflowRunning = false;
+
         public MainWindow()
         {
             InitializeComponent();
             RegisterMetadata();
             AddDesigner();
-            this.AddToolBox();
-            this.AddPropertyInspector();
+            AddToolBox();
+            AddPropertyInspector();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Handle the Loaded event here if needed
         }
 
         private void AddDesigner()
         {
-            // Create an instance of WorkflowDesigner class.
-            this.wd = new WorkflowDesigner();
+            wd = new WorkflowDesigner();
+            designerGrid.Children.Add(wd.View);
 
-            // Add the designer canvas to the designerGrid.
-            designerGrid.Children.Add(this.wd.View);
+            // Load a simple Sequence activity to ensure we have a valid workflow
+            wd.Load(new Sequence
+            {
+                Activities =
+                {
+                    new WriteLine { Text = "Hello, Workflow!" }
+                }
+            });
 
-            // Optionally, you can load an empty activity to start with no Sequence.
-            this.wd.Load(new ActivityBuilder { Name = "EmptyWorkflow" });
+            wd.ModelChanged += Wd_ModelChanged;
         }
 
         private void RegisterMetadata()
@@ -60,41 +57,20 @@ namespace Rehost_Again_
 
         private ToolboxControl GetToolboxControl()
         {
-            // Create the ToolBoxControl.
             var ctrl = new ToolboxControl();
-
-            // Create a category.
             var category = new ToolboxCategory("category1");
 
-            // Create Toolbox items.
-            var tool1 =
-                new ToolboxItemWrapper("System.Activities.Statements.Assign",
+            var tool1 = new ToolboxItemWrapper("System.Activities.Statements.Assign",
                 typeof(Assign).Assembly.FullName, null, "Assign");
-
             var tool2 = new ToolboxItemWrapper("System.Activities.Statements.Sequence",
                 typeof(Sequence).Assembly.FullName, null, "Sequence");
+            var tool3 = new ToolboxItemWrapper("System.Activities.Statements.WriteLine",
+                typeof(WriteLine).Assembly.FullName, null, "WriteLine");
 
-            var tool3 = new ToolboxItemWrapper("System.Activities.Statemen" +
-                "" + "ts.Sequence",
-               typeof(Sequence).Assembly.FullName, null, "Sequence");
-
-
-            var tool4 = new ToolboxItemWrapper("System.Activities.Statements.Sequence",
-               typeof(Sequence).Assembly.FullName, null, "Sequence");
-
-            var tool5 = new ToolboxItemWrapper("System.Activities.Statements.WriteLine",
-               typeof(WriteLine).Assembly.FullName, null, "WriteLine");
-
-       
-
-            // Add the Toolbox items to the category.
             category.Add(tool1);
             category.Add(tool2);
             category.Add(tool3);
-            category.Add(tool4);
-            category.Add(tool5);
-           
-            // Add the category to the ToolBox control.
+
             ctrl.Categories.Add(category);
             return ctrl;
         }
@@ -112,5 +88,138 @@ namespace Rehost_Again_
             grid1.Children.Add(wd.PropertyInspectorView);
         }
 
+        private void Wd_ModelChanged(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (wd != null && !isWorkflowRunning)
+                {
+                    btnPlay.IsEnabled = true;
+                    btnStop.IsEnabled = false;
+                }
+                else if (isWorkflowRunning)
+                {
+                    btnPlay.IsEnabled = false;
+                    btnStop.IsEnabled = true;
+                }
+                else
+                {
+                    btnPlay.IsEnabled = false;
+                    btnStop.IsEnabled = false;
+                }
+            });
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            RunWorkflow();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (wfApp != null)
+            {
+                wfApp.Cancel();
+
+                wfApp.Completed -= WfApp_Completed;
+                wfApp.Aborted -= WfApp_Aborted;
+                wfApp = null;
+
+                isWorkflowRunning = false;
+                UpdateUI();
+            }
+        }
+
+        private void btnRunWorkflow_Click(object sender, RoutedEventArgs e)
+        {
+            RunWorkflow();
+        }
+
+        private void RunWorkflow()
+        {
+            if (wd != null && wfApp == null)
+            {
+                var activity = wd.Context.Services.GetService<ModelService>().Root.GetCurrentValue() as Activity;
+
+                if (activity != null)
+                {
+                    wfApp = new WorkflowApplication(activity);
+
+                    wfApp.Completed += WfApp_Completed;
+                    wfApp.Aborted += WfApp_Aborted;
+                    wfApp.OnUnhandledException += WfApp_OnUnhandledException;
+
+                    wfApp.Extensions.Add(new TextWriterExtension(txtOutput));
+
+                    wfApp.Run();
+
+                    isWorkflowRunning = true;
+                    UpdateUI();
+                }
+                else
+                {
+                    MessageBox.Show("The workflow definition is null.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void WfApp_Completed(WorkflowApplicationCompletedEventArgs obj)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                isWorkflowRunning = false;
+                UpdateUI();
+            });
+        }
+
+        private void WfApp_Aborted(WorkflowApplicationAbortedEventArgs obj)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                isWorkflowRunning = false;
+                UpdateUI();
+            });
+        }
+
+        private UnhandledExceptionAction WfApp_OnUnhandledException(WorkflowApplicationUnhandledExceptionEventArgs arg)
+        {
+            MessageBox.Show($"Unhandled exception: {arg.UnhandledException.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return UnhandledExceptionAction.Terminate;
+        }
+
+        private class TextWriterExtension : System.IO.TextWriter
+        {
+            private readonly TextBox output;
+
+            public TextWriterExtension(TextBox output)
+            {
+                this.output = output;
+            }
+
+            public override void Write(char value)
+            {
+                output.Dispatcher.Invoke(() =>
+                {
+                    output.AppendText(value.ToString());
+                    output.ScrollToEnd();
+                });
+            }
+
+            public override void WriteLine(string value)
+            {
+                output.Dispatcher.Invoke(() =>
+                {
+                    output.AppendText(value + Environment.NewLine);
+                    output.ScrollToEnd();
+                });
+            }
+
+            public override System.Text.Encoding Encoding => System.Text.Encoding.UTF8;
+        }
     }
 }
