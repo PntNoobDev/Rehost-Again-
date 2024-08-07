d8315a6d735b5c9e21d8099465ebfadb3515adb1
PntNoobDev <phamngocthanh.d21ctc1@muce.edu.vn>
2024-08-07 11:29:51 +0700

    update fix convert Model Properties for SMTP Mail 7/8/2024

--- Rehost(Again)/ModelPropertyToModelItemConverter.cs
+++ Rehost(Again)/ModelPropertyToModelItemConverter.cs
@@ -1,22 +1,50 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 using System.Windows.Data;
 using System.Activities.Presentation.Model;
+using System.Activities.Expressions;
+using System.Activities;
+using System;
 
 namespace Rehost_Again_
 {
     public class ModelPropertyToModelItemConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            var modelProperty = value as ModelProperty;
-            return modelProperty?.ComputedValue;
+            if (value is InArgument<string>)
+            {
+                Activity<string> expression = ((InArgument<string>)value).Expression;
+                if (expression is Literal<string>)
+                {
+                    return ((Literal<string>)expression).Value;
+                }
+            }
+            var test = value as System.Activities.Presentation.Model.ModelItem;
+            if (test != null)
+            {
+                if (test.ItemType == typeof(InArgument<string>))
+                {
+                    var val = test.Properties["Expression"].Value;
+                    if (val != null)
+                    {
+                        var valstring = val.ToString();
+                        return valstring;
+                    }
+                    return val;
+                }
+            }
+            return null;
         }
-
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value is string)
+            {
+                return new InArgument<string>(new Literal<string>((string)value));
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
